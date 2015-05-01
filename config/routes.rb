5b096d1d920c5f8c00ce3a0770c3204b0b5d6e02
alexzwir <alexzwir@gmail.com>
2015-05-01 11:29:48 -0300

    Created home and about pages, and the navigation bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   
   #home do app
-  root 'pages#home'
+  root 'pages#home' 
   
-  get "about" => "pages#about"
+  get "about" => "pages#about" # creates about_path
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
